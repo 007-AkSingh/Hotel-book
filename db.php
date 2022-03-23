2b10abcba05d22f12d83eea4195f78c37d0f2b88
Aniket Singh <67497338+007-AkSingh@users.noreply.github.com>
2022-03-23 21:18:02 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,9 @@
 <?php
-$mysqli = new mysqli("remotemysql.com","o9E2yHQ2F6","UbldgHQyys","o9E2yHQ2F6");
+$mysqli = new mysqli("remotemysql.com","0IE4WTlfrT","GkM9WMhnIA","0IE4WTlfrT");
 
 // Check connection
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
 }
-else{
-  echo "Yep! Database Connected.";
 ?>
